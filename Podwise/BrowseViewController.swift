9d1ad4d38ce66c60c2be35895ed2f47a9b383e22
Jeff Chimney <jeffchimney@Jeffs-iMac.local>
2017-11-17 00:17:07 -0800

    Inital iTunes podcast directory search done

--- Podwise/BrowseViewController.swift
+++ Podwise/BrowseViewController.swift
@@ -9,19 +9,49 @@
 import Foundation
 import UIKit
 
-class BrowseViewController: UIViewController, UISearchBarDelegate {
+class BrowseViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBarBottomConstraint: NSLayoutConstraint!
+    var podcastResults: [[String: Any]] = []
+    var searchBarBottomConstraintInitialValue: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        searchBarBottomConstraintInitialValue = searchBarBottomConstraint.constant
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-
         iTunesSearch(term: searchBar.text!)
+        searchBar.resignFirstResponder()
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        let info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        UIView.animate(withDuration: duration) { () -> Void in
+            self.searchBarBottomConstraint.constant = 85-keyboardFrame.height
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        
+        UIView.animate(withDuration: duration) { () -> Void in
+            self.searchBarBottomConstraint.constant = self.searchBarBottomConstraintInitialValue
+            self.view.layoutIfNeeded()
+        }
     }
     
     //
@@ -36,7 +66,7 @@ class BrowseViewController: UIViewController, UISearchBarDelegate {
         let request: URLRequest = URLRequest(url: url)//NSURLRequest(URL: url as URL)
         let session: URLSession = URLSession.shared // NSURLConnection(request: rqst, delegate: self, startImmediately: false)!
         let task: URLSessionDataTask = session.dataTask(with: request, completionHandler: { (data, response, error) in
-            
+            self.ctnFinishedLoading(data: data!)
         })
         print("URL for searching iTunes API \(url)")
         task.resume()
@@ -47,13 +77,12 @@ class BrowseViewController: UIViewController, UISearchBarDelegate {
         // self.d should hold the resulting info, request is complete
         // received data is converted into an object through JSON deserialization
         do {
-            let jResult: Dictionary<String, Any> = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
-            if jResult.count > 0 {
-                let results = jResult["results"] as? [[String: Any]]
-                if results!.count > 0 {
-                    var results: NSArray = jResult["results"] as! NSArray
-                    self.tData = results
-                    self.appsTableView.reloadData()
+            let jResult: NSDictionary = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! NSDictionary
+            let results = jResult["results"] as! [[String: Any]]
+            if jResult.count > 0 && results.count > 0 {
+                self.podcastResults = results
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
                 }
             }
         } catch {
@@ -61,5 +90,42 @@ class BrowseViewController: UIViewController, UISearchBarDelegate {
         }
     }
     
-    Read more at http://technotif.com/connect-your-apps-to-itunes-search-api/#fC7VuBhzWqR8IcLe.99
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return podcastResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ResultCell", for: indexPath) as! PodcastCell
+        let rowData: [String: Any] = self.podcastResults[indexPath.row]
+        print(rowData)
+        cell.titleLabel.text = rowData["trackName"] as? String
+        cell.descriptionLabel.text = rowData["artistName"] as? String
+        cell.feedID = rowData["collectionId"] as? String
+        let urlString: String = rowData["artworkUrl60"] as! String
+        let url: URL = URL(string: urlString)!
+        let request: URLRequest = URLRequest(url: url)//NSURLRequest(URL: url as URL)
+        let session: URLSession = URLSession.shared // NSURLConnection(request: rqst, delegate: self, startImmediately: false)!
+        let task: URLSessionDataTask = session.dataTask(with: request, completionHandler: { (data, response, error) in
+            // Download representation of the image as NSData at the URL
+            do {
+                let imageData: Data = try Data.init(contentsOf: url, options: .mappedIfSafe)
+                DispatchQueue.main.async {
+                    cell.artImageView.image = UIImage(data: imageData)
+                }
+            } catch {
+                print("Could not load art thumbnail")
+            }
+        })
+        print("URL for searching iTunes API \(url)")
+        task.resume()
+        
+        cell.artImageView.layer.cornerRadius = 10
+        cell.artImageView.layer.masksToBounds = true
+
+        return cell
+    }
 }
--- Podwise/New Group/PodcastCell.swift
+++ Podwise/New Group/PodcastCell.swift
@@ -12,4 +12,8 @@ import UIKit
 class PodcastCell: UITableViewCell {
     
     @IBOutlet weak var artImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    var feedID: String!
+    
 }
